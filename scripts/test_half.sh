148d6b11a04032f21b533dadea4c5a96368910ff
Ubuntu <ubuntu@ip-172-31-26-251.us-west-2.compute.internal>
2020-01-06 03:43:37 +0000

    Update scripts.

--- scripts/test_half.sh
+++ scripts/test_half.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python test.py --dataroot ./datasets/fake --name 6_half_30x30 --model test --which_epoch latest --which_model_netG resnet_2x_6blocks --which_direction AtoB --dataset_mode single --norm batch --resize_or_crop none --fineSize 256 --gpu_ids 0
+python test.py --dataroot ./datasets/half/500 --name 500_half_style_256x1 --model test --which_epoch latest --which_model_netG resnet_2x_6blocks --which_direction AtoB --dataset_mode single --norm batch --resize_or_crop none --fineSize 256 --gpu_ids 0
